1317a207e0278b9ab51c7cd895dc7edd034a898b
uehara <b1015062@fun.ac.jp>
2018-11-21 11:33:31 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='askfmcrawler',
-      version='0.1.1',
+      version='0.1.2',
       description='Python askfm crawler.',
       author='uehara1414',
       author_email='akiya.noface@gmail.com',
